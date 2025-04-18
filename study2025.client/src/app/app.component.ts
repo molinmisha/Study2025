936b1597b677a61c406f65bad346d6d28a7367b9
misha <ultramolin@gmail.com>
2025-04-18 13:17:32 +0300

    update to 19

--- study2025.client/src/app/app.component.ts
+++ study2025.client/src/app/app.component.ts
@@ -18,9 +18,10 @@ interface WeatherForecast {
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 export class AppComponent implements OnInit {
   public forecasts: WeatherForecast[] = [];
